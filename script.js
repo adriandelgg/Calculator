3ae366a050a96808e9467c7b44e82bf25eefdfa3
adriandelgg <adriandelgadoc1@gmail.com>
2021-02-08 20:12:05 -0800

    Completed calc & functionality, just needs color

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-// Change the total-number input with what I click
-// Make clear clear input and back to 0
-// 
+// Calculator
+const total = document.querySelector('h3');
 
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const seven = document.querySelector('.seven');
-const eight = document.querySelector('.eight');
-const nine = document.querySelector('.nine');
-const zero = document.querySelector('.zero');
-
-const equals = document.querySelector('.equals');
-const add = document.querySelector('.add');
-const subtract = document.querySelector('.subtract');
-const posNeg = document.querySelector('.pos-neg');
-const clear = document.querySelector('.clear');
-const decimal = document.querySelector('.decimal');
-const divide = document.querySelector('.divide');
-const multiply = document.querySelector('.multiply');
+function calculateTotal(obj) {
+    const pushed = obj.innerHTML;
+    
+    if (pushed == '=') {
+        total.innerHTML = eval(total.innerHTML);
+    
+    } else if (pushed == 'C') {
+        total.innerHTML = '0';
+    
+    } else {
+        if (total.innerHTML == '0') {
+            total.innerHTML = pushed;
+        
+        } else {
+            total.innerHTML += pushed;
+        }
+    }
+}
