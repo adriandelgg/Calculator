f26fdd51d16c455ef7580fe8edbf7cb2916c275a
adriandelgg <adriandelgadoc1@gmail.com>
2021-02-08 18:01:08 -0800

    Finished making the UI

---
+++ script.js
@@ -0,0 +1,23 @@
+// Change the total-number input with what I click
+// Make clear clear input and back to 0
+// 
+
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+
+const equals = document.querySelector('.equals');
+const add = document.querySelector('.add');
+const subtract = document.querySelector('.subtract');
+const posNeg = document.querySelector('.pos-neg');
+const clear = document.querySelector('.clear');
+const decimal = document.querySelector('.decimal');
+const divide = document.querySelector('.divide');
+const multiply = document.querySelector('.multiply');
